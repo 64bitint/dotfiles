d16b9cc694a8033962b6fa30f6e222274c62e1a2
64bitint <64bitint@users.noreply.github.com>
2022-08-14 20:01:01 -0400

    update

--- .config/yadm/install-posh.sh
+++ .config/yadm/install-posh.sh
@@ -13,7 +13,3 @@ esac
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-$posh_arch \
 	-O /usr/local/bin/oh-my-posh && \
 	sudo chmod +x /usr/local/bin/oh-my-posh
-
-sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-arm64 \
-	-O /usr/local/bin/oh-my-posh && \
-		sudo chmod +x /usr/local/bin/oh-my-posh
