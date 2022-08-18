f6851b9ac0beec008d534a8ea761c8b761256600
64bitint <64bitint@users.noreply.github.com>
2022-08-18 02:33:29 -0400

    add -p to mkdir

--- .config/yadm/install-yadm.sh
+++ .config/yadm/install-yadm.sh
@@ -4,7 +4,7 @@
 # apk add bash git
 # apt install wget bash git
 
-mkdir ~/.local/bin && \
+mkdir -p ~/.local/bin && \
 wget -O ~/.local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && \
 chmod +x ~/.local/bin/yadm && \
 ~/.local/bin/yadm clone --bootstrap https://github.com/64bitint/dotfiles.git
