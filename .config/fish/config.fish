1f2670ecd84334bed090a3ae557cb9aca91d0281
Kevin Muma <kevinmuma@gmail.com>
2022-10-10 19:40:04 +0000

    prevent errors on fish < v3.4.0

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     export MICRO_TRUECOLOR=1
     if type -q oh-my-posh
-        oh-my-posh init fish --config ~/.poshthemes/mytheme.omp.json | source
+        oh-my-posh init fish --config ~/.poshthemes/mytheme.omp.json | \
+        sed 's/test "\$(\(.*\))" != \'\'/test -z (echo (\1))/' | sed 's/$(/(/' | \
+        source
     end
     abbr -a ll "ls -alF"
     if not type -q sudo; and type -q doas
